e678dc7b91e3b1746b76996cb11f0e1330fb1d4b
Nag <nagarjuna.chirala@gmail.com>
2018-04-17 00:42:04 -0700

    initial checkin

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=nags-csharp-luis-bot-Web-Deploy.pubxml -p:Password=7m1svhgeplZFwTerTwP7eqlycYbRrw8kT0WMoBM6Q0qJbAzT97xAfhDkdo97
+
